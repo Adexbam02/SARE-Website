6a9e7be342e67da9b52f348c0e3585fdb9ce8c3c
Adexbam02 <nurainbamidelejnr@gmail.com>
2026-07-16 15:41:59 +0100

    feat: initialize core website structure, site navigation, and project-specific data modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
+import { headers } from "next/headers";
 import "./globals.css";
 import { spaceGrotesk } from "../../public/fonts/fonts";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export const metadata: Metadata = {
   title: "SARE - Society of Agricultural Robotic Engineers",
@@ -10,18 +11,22 @@ export const metadata: Metadata = {
     "Bringing together innovators in Agricultural & Environmental Engineering to create real-world impact",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headersList = await headers();
+  const host = headersList.get("host") || "";
+  const isCtrlSubdomain = host.startsWith("ctrl.");
+
   return (
     <html lang="en">
-      <body className={`${spaceGrotesk.className}  antialiased overflow-x-hidden`}>
-        <Navbar />
-        <div className="h-max w-full">
-          {children}
-        </div>
+      <body
+        className={`${spaceGrotesk.className}  antialiased overflow-x-clip`}
+      >
+        {!isCtrlSubdomain && <Navbar />}
+        <div className="h-max w-full">{children}</div>
         <Footer />
       </body>
     </html>
