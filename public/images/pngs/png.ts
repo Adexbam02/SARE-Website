e2feb2a6fb408d5f782323a045307d5a3baa437e
Adexbam02 <nurainbamidelejnr@gmail.com>
2026-07-17 10:06:42 +0100

    feat: implement Events page with filtering, category selection, and footer component

--- public/images/pngs/png.ts
+++ public/images/pngs/png.ts
@@ -1,4 +1,5 @@
 import CTRLLABS_LOGO from "./CTRL logo.png";
 import EventsImg from "./carousel.png";
+import HeroImg from "./eventsBg.png";
 
-export { CTRLLABS_LOGO, EventsImg };
+export { CTRLLABS_LOGO, EventsImg, HeroImg };
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,10 @@
 import AboutHeader from "@/components/AboutHeader";
 import { EventsImg } from "../../../public/images/pngs/png";
+import Hero from "@/components/events/Hero";
+
+import Events from "@/components/events/Events";
+import FAQs from "@/components/FAQs";
+import Newsletter from "@/components/Newsletter";
 
 function page() {
   return (
@@ -9,6 +14,10 @@ function page() {
         title="EVENTS & ACTIVITIES"
         description="Bringing together students, innovators, and leaders in Agricultural & Environmental Engineering to create real-world impact"
       />
+      <Hero />
+      <Events />
+      <FAQs />
+      <Newsletter />
     </div>
   );
 }
